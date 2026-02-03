da1b66eae7d2d6509d4a2784a20b7c17fa561765
Nishant Jha <nishant.jha.aiml.2022@mitmeerut.ac.in>
2026-02-03 23:36:57 +0530

    "Apihandler Created"

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ dotenv.config({
     path: "./.env"
 });
 import connectDB from "./db/index.js";
-const PORT = process.env.PORT || 8000;
+import { app } from "./app.js";
+const PORT = process.env.PORT || 5000;
 
 connectDB()
+.then(app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}))
+.catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+});
 
 
 
