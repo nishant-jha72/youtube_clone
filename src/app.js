4c9d2228b53ab022e49a8e371a04606cdef08554
Nishant Jha <nishant.jha.aiml.2022@mitmeerut.ac.in>
2026-02-04 23:59:40 +0530

    "fixed bug"

--- src/app.js
+++ src/app.js
@@ -11,4 +11,8 @@ app.use(express.json({ limit: "50kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static("public"));
+
+// routes import 
+import userRoutes from "./routes/user.routes.js";
+app.use("/api/v1/users", userRoutes);
 export { app };
