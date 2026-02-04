785d3adbae694e76fae70f784e5cdd8b097be797
Nishant Jha <nishant.jha.aiml.2022@mitmeerut.ac.in>
2026-02-05 02:15:55 +0530

    "Connecting Routes Sample"

--- src/app.js
+++ src/app.js
@@ -11,7 +11,9 @@ app.use(express.json({ limit: "50kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static("public"));
-
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Server is running..." });
+});
 // routes import 
 import userRoutes from "./routes/user.routes.js";
 app.use("/api/v1/users", userRoutes);
