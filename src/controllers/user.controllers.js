1f8d515253624c20f2de42b2a5a268afc82a8245
Nishant Jha <nishant.jha.aiml.2022@mitmeerut.ac.in>
2026-02-07 16:57:00 +0530

    "Created Login And Logout Routes"

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -115,8 +115,33 @@ const login = asyncHandler( async (req , res) => {
       "User Logged in succesFully"
     )
   )
-
 });
+
+const logoutUser = asyncHandler(async(req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset: {
+                refreshToken: 1 // this removes the field from document
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out"))
+})
 export { registerUser  ,
-          login
+          login ,
+          logoutUser
 };
